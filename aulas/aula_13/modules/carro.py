1469889cd03a3f7bd0eeecaff3897df360b7d249
AureliaCovre <93683314+AureliaCovre@users.noreply.github.com>
2022-11-08 11:20:11 -0300

    Update carro.py

--- aulas/aula_13/modules/carro.py
+++ aulas/aula_13/modules/carro.py
@@ -3,10 +3,15 @@ from modules.veiculo import Veiculo
 class Carro(Veiculo):
     
     #vel_max = 0
+    fabricante = ""
     motor = ""
+    nome = "nome do carro"
         
-    # def print_vel_max(self):
-    #     print (self.vel_max)
+    def print_vel_max(self):
+        print ("velocidade do carro ", self.vel_max)
     
     def print_motor(self):
         print(self.motor)
+        
+    def print_nome(self):
+        super().print_nome()
