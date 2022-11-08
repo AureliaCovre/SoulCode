ed2302ad17c6f16cb4bfc77b2bffe2f74246c8bb
AureliaCovre <93683314+AureliaCovre@users.noreply.github.com>
2022-11-08 11:17:57 -0300

    Create carro.py

---
+++ aulas/aula_13/modules/carro.py
@@ -0,0 +1,12 @@
+from modules.veiculo import Veiculo
+
+class Carro(Veiculo):
+    
+    #vel_max = 0
+    motor = ""
+        
+    # def print_vel_max(self):
+    #     print (self.vel_max)
+    
+    def print_motor(self):
+        print(self.motor)
