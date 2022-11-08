3cfacca7ef4d03067035f17db08752175695d455
AureliaCovre <93683314+AureliaCovre@users.noreply.github.com>
2022-11-08 11:19:47 -0300

    Update veiculo.py

--- aulas/aula_13/modules/veiculo.py
+++ aulas/aula_13/modules/veiculo.py
@@ -1,5 +1,9 @@
 class Veiculo:
     vel_max = 0
+    nome = "nome do veiculo"
     
+    def print_nome(self):
+        print (self.nome)
+            
     def print_vel_max(self):
         print (self.vel_max)
