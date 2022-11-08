caa8253fb6dce0d9cb94ad6d5673e61369123625
AureliaCovre <93683314+AureliaCovre@users.noreply.github.com>
2022-11-08 12:38:34 -0300

    Update connector.py

--- ATIVIDADES/Exercicio_Cassandra/modules/connector.py
+++ ATIVIDADES/Exercicio_Cassandra/modules/connector.py
@@ -1,1 +1,20 @@
-sss
+__author__ = "André Ventura"
+
+from cassandra.cluster import Cluster
+
+class CassandraConector:
+    
+    def __init__(self,keyspace:str):
+        self.cluster = Cluster()
+        self.session = self.cluster.connect(keyspace)
+        
+    def insert(self,tipo:int=0,familia:str='',dicionarios=''):
+        
+            for dicionario in dicionarios:
+                keys = list(dicionario.keys())
+                values = list(dicionario.values())
+                query = f"INSERT INTO {familia} ({','.join(keys)}) VALUES ({','.join(values)});"
+                # print(query)
+                self.session.execute(query)
+               
+            print('inseridos',len(dicionarios))
