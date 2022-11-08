b6a4f18dc83780d4ec210dc8f8bed0ca21cd6a55
AureliaCovre <93683314+AureliaCovre@users.noreply.github.com>
2022-11-08 11:29:38 -0300

    Add files via upload

---
+++ aulas/aula_14/__main__.py
@@ -0,0 +1,9 @@
+from modules.connector import Interface_db
+
+#Crie uma classe funcionario, instancie 5 funcionarios, calcule e apresente para cada funcionario:
+#- quantas horas extras que cada funcionario fez neste mes
+#- o total de vendas neste mes para cada funcionario
+#- se o contrato do funcionário se encerra neste mes
+
+if __name__ == "__main__":
+   interface_mongo = Interface_db()
